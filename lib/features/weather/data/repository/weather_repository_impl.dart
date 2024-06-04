243d8354d2c304d40888a9978d2de02b07ddf973
anuj-t21 <52398324+anuj-t21@users.noreply.github.com>
2024-06-04 16:32:48 +0530

    Comment added for repository impl

--- lib/features/weather/data/repository/weather_repository_impl.dart
+++ lib/features/weather/data/repository/weather_repository_impl.dart
@@ -7,10 +7,13 @@ import 'package:liveful_task/features/weather/domain/entities/weather.dart';
 import 'package:liveful_task/features/weather/domain/repository/weather_repository.dart';
 import 'package:http/http.dart' as http;
 
+// Implementation of the WeatherRepository interface.
 class WeatherRepositoryImpl implements WeatherRepository {
+  // Fetches weather data for a given city using the OpenWeatherMap API.
   @override
   Future<Weather> fetchWeatherData({required String city}) async {
     try {
+      // Construct the URL with query parameters.
       var url = Uri.https(
         'api.openweathermap.org',
         '/data/2.5/weather',
@@ -20,19 +23,26 @@ class WeatherRepositoryImpl implements WeatherRepository {
           "appid": AppSecrets.weatherApiKey,
         },
       );
+      // Perform the HTTP GET request.
       final http.Response response = await http.get(url);
+      // Log the response body for debugging purposes.
       log(response.body.toString());
+      // Check the response status code and parse the JSON if successful.
       if (response.statusCode == 200) {
         final Map<String, dynamic> decodedJson = json.decode(response.body);
+        // Convert the JSON map into a WeatherModel object.
         return WeatherModel.fromJson(decodedJson);
       } else if (response.statusCode == 404) {
+        // Handle the case where the city is not found.
         final Map<String, dynamic> decodedJson = json.decode(response.body);
         throw decodedJson['message'];
       } else {
+        // Log unexpected status codes.
         log(response.statusCode.toString());
         throw 'Failed to load weather data';
       }
     } catch (e) {
+      // Handle exceptions that occur during the fetch operation.
       throw Exception(e);
     }
   }
